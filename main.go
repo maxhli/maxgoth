bb15a3468699ce7163d43a44e32ba2ecccb5e71b
maxli_thinkpade570 <max.li@live.com>
2018-01-24 10:48:53 -0600

    Revert "isProd to "true or false""
    This reverts commit a1876eb

--- main.go
+++ main.go
@@ -58,7 +58,7 @@ func main() {
 
 	key := os.Getenv("SESSION_SECRET")             // Replace with your SESSION_SECRET or similar
 	maxAge := 86400 * 30  // 30 days
-	isProd := true       // Set to true when serving over https (false/true)
+	isProd := false       // Set to true when serving over https
 
 	store := sessions.NewCookieStore([]byte(key))
 	store.MaxAge(maxAge)
