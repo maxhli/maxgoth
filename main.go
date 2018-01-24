f7a810a2e746fc63b1899ed1e26975f808c966e0
maxli_thinkpade570 <max.li@live.com>
2018-01-24 09:02:10 -0600

    work with Gothic.store

--- main.go
+++ main.go
@@ -51,9 +51,23 @@ import (
 	"github.com/markbates/goth/providers/xero"
 	"github.com/markbates/goth/providers/yahoo"
 	"github.com/markbates/goth/providers/yammer"
+	"github.com/gorilla/sessions"
 )
 
 func main() {
+
+	key := os.Getenv("SESSION_SECRET")             // Replace with your SESSION_SECRET or similar
+	maxAge := 86400 * 30  // 30 days
+	isProd := false       // Set to true when serving over https
+
+	store := sessions.NewCookieStore([]byte(key))
+	store.MaxAge(maxAge)
+	store.Options.Path = "/"
+	store.Options.HttpOnly = true   // HttpOnly should always be enabled
+	store.Options.Secure = isProd
+
+	gothic.Store = store
+
 	goth.UseProviders(
 		twitter.New(os.Getenv("TWITTER_KEY"), os.Getenv("TWITTER_SECRET"), "http://localhost:3000/auth/twitter/callback"),
 		// If you'd like to use authenticate instead of authorize in Twitter provider, use this instead.
