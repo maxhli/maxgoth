a1876eb51bb28799f06338daf96154b0195c4fd1
maxli_thinkpade570 <max.li@live.com>
2018-01-24 09:08:02 -0600

    isProd to "true or false"

--- main.go
+++ main.go
@@ -58,7 +58,7 @@ func main() {
 
 	key := os.Getenv("SESSION_SECRET")             // Replace with your SESSION_SECRET or similar
 	maxAge := 86400 * 30  // 30 days
-	isProd := false       // Set to true when serving over https
+	isProd := true       // Set to true when serving over https (false/true)
 
 	store := sessions.NewCookieStore([]byte(key))
 	store.MaxAge(maxAge)
